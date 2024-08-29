26242a7588313f66f3b3a836e0f70d7be7b0592e
rach-18 <rachitapradhan448@gmail.com>
2024-08-30 01:11:29 +0530

    Made the about and contact section responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,23 +8,25 @@ function About() {
         {
             "content" : 
             <div className="slide" key={0}>
-                <p className='2xl:text-5xl lg:text-4xl text-2xl font-bold mb-2'>Keysight Technologies</p>
-                <div className='flex justify-between 2xl:text-4xl lg:text-2xl text-xl font-semibold'>
+                <p className='2xl:text-5xl lg:text-4xl sm:text-2xl sm:text-left text-center text-xl font-bold mb-2'>Keysight Technologies</p>
+                <div className='flex sm:justify-between items-center sm:flex-row flex-col 2xl:text-4xl lg:text-2xl sm:text-xl text-base font-semibold'>
                     <p>Research and Development Intern</p>
                     <p>Jan 2023 - Jul 2023</p>
                 </div>
-                <div className='flex gap-2 items-center my-2 2xl:text-xl text-sm'>
+                <div className='flex gap-2 items-center my-2 2xl:text-xl sm:text-sm text-xs'>
                     <i className="fa-solid fa-location-dot"></i>
                     <p>Kolkata, India</p>
                 </div>
                 {/* <p className='text-xl underline'>Description:</p> */}
-                <ul className='list-disc ml-8 mt-3 flex flex-col gap-2 2xl:text-2xl'>
-                    <li>Improved the functionality of IxNetwork Web Edition by collaborating with a team of 5 to
+                <ul className='list-disc ml-8 mt-3 flex flex-col gap-2 2xl:text-2xl sm:text-base text-sm'>
+                    {/* <li>Improved the functionality of IxNetwork Web Edition by collaborating with a team of 5 to
                     optimize the network. monitoring tool.</li>
                     <li>Conducted manual debugging of over 200 test cases, resolving software issues for smooth
                     functionality and optimal performance.</li>
                     <li>Documented project changes and updates in team manuals, providing valuable insights for
-                    future development.</li>
+                    future development.</li> */}
+                    <li>Integrated new features into React components, including advanced state management and conditional rendering, reducing feature implementation time by 20%.</li>
+                    <li>Assisted in manually debugging 50+ test cases, leading to the identification and resolution of software issues, significantly improving code quality and application performance.</li>
                 </ul>
             </div>,
             "logo" : "https://imgs.search.brave.com/OuLcTXVvhQTDJcSdjiLBemZOxHs4KC5IXZUKX54eNbk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jb21w/YW5pZXNsb2dvLmNv/bS9pbWcvb3JpZy9L/RVlTLTE4YTM0NjY5/LnBuZz90PTE2NDg1/NzIyMTk"
@@ -32,20 +34,22 @@ function About() {
         {
             "content" : 
             <div className="slide" key={1}>
-                <p className='2xl:text-5xl text-4xl font-bold mb-2'>Variable Energy Cylotron Center (VECC)</p>
-                <div className='flex justify-between 2xl:text-4xl text-2xl font-semibold'>
+                <p className='2xl:text-5xl lg:text-4xl sm:text-2xl sm:text-left text-center text-xl font-bold mb-2'>Variable Energy Cylotron Center (VECC)</p>
+                <div className='flex sm:justify-between items-center sm:flex-row flex-col 2xl:text-4xl lg:text-2xl sm:text-xl text-base font-semibold'>
                     <p>Web Development Intern</p>
                     <p>Jun 2022 - Jul 2022</p>
                 </div>
-                <div className='flex gap-2 items-center my-2 2xl:text-xl text-sm'>
+                <div className='flex gap-2 items-center my-2 2xl:text-xl sm:text-sm text-xs'>
                     <i className="fa-solid fa-location-dot"></i>
                     <p>Kolkata, India</p>
                 </div>
                 {/* <p className='text-xl underline'>Description:</p> */}
-                <ul className='list-disc ml-8 mt-3 flex flex-col gap-2 2xl:text-2xl'>
-                    <li>Revamped the organization's recruitment page to improve user experience and responsiveness.</li>
+                <ul className='list-disc ml-8 mt-3 flex flex-col gap-2 2xl:text-2xl sm:text-base text-sm'>
+                    {/* <li>Revamped the organization's recruitment page to improve user experience and responsiveness.</li>
                     <li>Leveraged HTML, CSS, and Bootstrap to redesign the portal, resulting in a modern and user-friendly interface.</li>
-                    <li>Ensured the website's responsiveness for multiple devices, enhancing accessibility for candidates.</li>
+                    <li>Ensured the website's responsiveness for multiple devices, enhancing accessibility for candidates.</li> */}
+                    <li>Revamped the outdated UI of the recruitment page by restructuring and modernizing the codebase, enhancing maintainability and readability, which led to a 30% reduction in code-related issues.</li>
+                    <li>Implemented Bootstrap to redesign the page, achieving a responsive layout 100% that improved user experience across devices, resulting in a 40% increase in user engagement.</li>
                 </ul>
             </div>,
             "logo" : "https://imgs.search.brave.com/wjrwXF_FsEXZQ8uuZVTXKGm-BhMmgUQRI9LhdCmNP3A/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Ni82NS9WYXJpYWJs/ZV9FbmVyZ3lfQ3lj/bG90cm9uX0NlbnRy/ZS5zdmcvMjIwcHgt/VmFyaWFibGVfRW5l/cmd5X0N5Y2xvdHJv/bl9DZW50cmUuc3Zn/LnBuZw"
@@ -83,26 +87,26 @@ function About() {
                     <img className="rounded-full" src="avatar.png" alt="avatar" />
                 </div>
             </div>
-            <div id="experience" className="2xl:h-[85vh] h-screen flex flex-col lg:gap-10 gap-5 relative bg-[url(experience-bg.png)]">
+            <div id="experience" className="2xl:h-[85vh] h-screen flex flex-col lg:gap-10 sm:gap-5 gap-2 relative bg-[url(experience-bg.png)]">
                 <div className="flex items-center justify-center">
-                    <p className="bg-[#006837] 2xl:mt-20 mt-12 px-4 py-2 2xl:text-2xl lg:text-xl text-base  rounded-full text-white">EXPERIENCE</p>
+                    <p className="bg-[#006837] 2xl:mt-20 sm:mt-12 mt-10 px-4 py-2 2xl:text-2xl lg:text-xl text-base  rounded-full text-white">EXPERIENCE</p>
                 </div>
                 <div className="flex flex-col justify-around">
-                    <div className="mx-auto w-[80%] lg:mt-5">
+                    <div className="mx-auto sm:w-[80%] w-11/12 lg:mt-5">
                         <div className="carousel-content text-lime-950">
                             {exp[currentIndex].content}
                         </div>
                     </div>
-                    <div id="carousel" className="flex items-end justify-center lg:text-lg lg:py-5 py-3 absolute bottom-0 w-full">
+                    <div id="carousel" className="flex items-end justify-center lg:text-lg sm:text-base text-sm lg:py-5 py-3 absolute bottom-0 w-full">
                         <div className="flex items-center justify-evenly w-full">
-                            <div onClick={prevSlide} className="bg-white lg:w-[5rem] w-[4rem] lg:h-[5rem] h-[4rem] flex items-center justify-center rounded-full shadow-xl cursor-pointer hover:opacity-60">
+                            <div onClick={prevSlide} className="bg-white lg:w-[5rem] sm:w-[4rem] w-[3rem] lg:h-[5rem] sm:h-[4rem] h-[3rem] flex items-center justify-center rounded-full shadow-xl cursor-pointer hover:opacity-60">
                                 <i className="fa-solid fa-arrow-left"></i>
                             </div>
-                            <div className="bg-white lg:w-[7.5rem] w-[6rem] lg:h-[7.5rem] h-[6rem] flex items-center justify-center rounded-full text-center shadow-xl">
+                            <div className="bg-white lg:w-[7.5rem] sm:w-[6rem] w-[5rem] lg:h-[7.5rem] sm:h-[6rem] h-[5rem] flex items-center justify-center rounded-full text-center shadow-xl">
                                 {/* <p>{exp[currentIndex].logo}</p> */}
                                 <img className='w-[80%]' src={exp[currentIndex].logo} alt="" />
                             </div>
-                            <div onClick={nextSlide} className="bg-white lg:w-[5rem] w-[4rem] lg:h-[5rem] h-[4rem] flex items-center justify-center rounded-full shadow-xl cursor-pointer hover:opacity-60">
+                            <div onClick={nextSlide} className="bg-white lg:w-[5rem] sm:w-[4rem] w-[3rem] lg:h-[5rem] sm:h-[4rem] h-[3rem] flex items-center justify-center rounded-full shadow-xl cursor-pointer hover:opacity-60">
                                 <i className="fa-solid fa-arrow-right"></i>
                             </div>
                         </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,24 +4,24 @@ function Contact() {
             <div id="contact-signboard" data-aos="fade-down" data-aos-duration="1500" data-aos-once="true">
                 <div>
                     <div className="relative flex justify-center">
-                        <div className="flex gap-[5rem] absolute top-[-1.8rem] z-[2]">
+                        <div className="flex lg:gap-[6rem] gap-[5rem] absolute top-[-1.8rem] z-[2]">
                             <img id="rope-clip" src="rope.png" alt="" />
                             <img id="rope-clip" className="scale-x-[-1]" src="rope.png" alt="" />
                         </div>
-                        <p className="bg-[#F0C0A9] text-white inline px-8 py-2 text-xl rounded-full absolute top-[3rem] z-[1] text-center">CONTACT ME</p>
+                        <p className="bg-[#F0C0A9] text-white inline px-8 py-2 2xl:text-2xl lg:text-xl text-base rounded-full absolute top-[3rem] z-[1] text-center">CONTACT ME</p>
                     </div>
                 </div>
             </div>
             <div>
                 <div className="flex flex-col gap-5 items-center justify-center pt-32">
-                    <img className="profile-pic w-[12%] rounded-full shadow-lg" src="me.jpeg" alt="" />
-                    <p className="bg-white text-xl px-6 py-2 rounded-full">Let's Connect!</p>
+                    <img className="profile-pic lg:w-[12%] md:w-[18%] sm:w-[20%] w-[26%] rounded-full shadow-lg" src="me.jpeg" alt="" />
+                    <p className="bg-white sm:text-xl text-lg px-6 py-2 rounded-full">Let's Connect!</p>
                 </div>
-                <form action="" className="flex flex-col py-10 w-5/6 mx-auto gap-5">
-                    <input className="p-4 rounded-lg shadow-md" type="text" placeholder="Name..." />
-                    <input className="p-4 rounded-lg shadow-md" type="text" placeholder="Email..." />
-                    <textarea className="p-4 rounded-lg shadow-md" rows="5" placeholder="Message..."></textarea>
-                    <button id="submit-btn" className="bg-[#A8C090] hover:bg-[#B7CFAF] text-white text-xl font-bold w-[20%] mx-auto py-3 rounded-lg" type="submit">SUBMIT</button>
+                <form action="" className="flex flex-col pb-10 sm:pt-10 pt-8 w-5/6 mx-auto sm:gap-5 gap-3">
+                    <input className="sm:p-4 p-3 rounded-lg shadow-md" type="text" placeholder="Name..." />
+                    <input className="sm:p-4 p-3 rounded-lg shadow-md" type="text" placeholder="Email..." />
+                    <textarea className="sm:p-4 p-3 rounded-lg shadow-md" rows="5" placeholder="Message..."></textarea>
+                    <button id="submit-btn" className="bg-[#A8C090] hover:bg-[#B7CFAF] text-white sm:text-xl font-bold sm:w-[20%] w-[25%] mx-auto py-3 rounded-lg" type="submit">SUBMIT</button>
                 </form>
             </div>
         </div>
